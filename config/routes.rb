a43fb2f985d9f5b057dffcdd37e90c4229d816d0
Maria Dumitrita Tomsa <maria.tomsa@protonmail.com>
2024-03-05 14:49:29 +0000

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/new", to: "tasks#new"
+  delete "tasks/:id", to: "tasks#destroy"
 end
